41d0f2fa2570e2af12f93df5062355ea6930ff26
Trey Rader <treyrdr09@gmail.com>
2017-01-26 16:15:28 -0600

    added variable to determine which gallery will display in its view

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,5 +1,8 @@
 app.controller('MainCtrl', ['$scope', function($scope, $http) {
 
+  
+  $scope.gallery_1 = true;
+
 	$scope.openNav = function() {
 		document.getElementById("mySidenav").style.width = "60%";
    		document.getElementById("mySidenav").style.display = "block";
@@ -50,53 +53,4 @@ app.controller('MainCtrl', ['$scope', function($scope, $http) {
  			$(this).next('p').slideToggle();//the p after the first p
   		})
   	});
- 
- 
-/*	$(document).ready(function() {
- 	
-  
-  	
-
-
- 	//iterate thru each container of images and select the first p
-  
-  
-  		$scope.imageColumn.each(function(index, column) { //for each column, find the image
-
-  			var $image = $(column).find('img');
-  			$image.click(function() {
-  				var img_src = $(this).data("modal-image")
-  				var requirements = $(this).data("requirements")
-  				var img = '<a href="' +modal_href+ '" target="_blank"><img class="mySlides" src="' + img_src + '" style="width:100%"></a>';
-  				$scope.modalImagesList.append(img);
- 				$scope.modalRequirements.html(requirements)
- 				$scope.modal.fadeIn(500);
- 			});
-		});
-
-
-		$scope.closeBtn.click(function() {
- 			$scope.modal.fadeOut(500, function() {
-  				$(this).find('img').remove();
-  				$scope.modalRequirements.find('li').remove();
-  			});
-		});
-
-
- 		$scope.imageRow.each(function(index, row) { //there are two rows
- 		
- 			$(row).find('img').mouseenter(function() {
-  				$(this).next().find('p:first-child').next('p').show('fold');
-  			})
-  			$(row).find('p:first-child').click(function() {
- 				$(this).next('p').slideToggle();
- 				$(this).next('p').slideToggle();//the p after the first p
-  			})
-  		});
- 	});
-
-
-*/
-
-
 }]);
