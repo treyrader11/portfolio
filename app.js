1588a359da3f1eb6915f11e0077a5f8328164d76
Trey Rader <treyrdr09@gmail.com>
2017-01-26 00:48:10 -0600

    added second gallery--anguklar projects

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var app = angular.module('portfolio', ['ngAnimate', 'ngRoute']);
 
 
-/*app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	
-	$locationProvider.html5Mode(true);
+	//$locationProvider.html5Mode(true);
 
 	$routeProvider
-	.when('/home', {
-		templateUrl: 'views/home.html',
+	.when('/', {
+		templateUrl: 'views/gallery_one.html',
 		controller: 'MainCtrl'
 	})
-	.otherwise({
-		redirectTo: 'index.html'
-	});
+	.when('/gallery_two', {
+		templateUrl: 'views/gallery_two.html',
+		controller: 'MainCtrl'
+	})
+	.otherwise('/');
 
-}]);*/
+}]);
 
 
 
